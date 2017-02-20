af15d16e3250ebdb742e2793e8793d81f4933908
taczc64 <tangzc64@139.com>
2017-02-21 01:23:23 +0800

    get all nodes and run runtine

--- main.go
+++ main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/cihub/seelog"
+	"os"
 )
 
 type node struct {
-    name:             string  `json:"name"`
-    host:             string  `json:"host"`
-    port:             int32   `json:"port"`
-    worker_name:      string  `json:"worker_name"`
-    worker_password:  string  `json:"worker_password"`
-    enable:           bool    `json:"enable"`
-    debug:            bool    `json:"debug"`
+	Name           string `json:"name"`
+	Host           string `json:"host"`
+	Port           int32  `json:"port"`
+	WorkerName     string `json:"worker_name"`
+	WorkerPassword string `json:"worker_password"`
+	Enable         bool   `json:"enable"`
+	Debug          bool   `json:"debug"`
 }
 
-type nodes struct {
-	Nodes []node
+type pools struct {
+	Nodes []node `json:"nodes"`
 }
 
-func loadNodes() {
+func loadNodes(config *pools) {
+	filename := "nodes.json"
+	seelog.Info("loading nodes file:", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		seelog.Error("cannot find nodes file:", err.Error())
+		panic(err.Error())
+	}
+	defer file.Close()
 
+	jsonParse := json.NewDecoder(file)
+	if err := jsonParse.Decode(&config); err != nil {
+		seelog.Critical("Config error: ", err.Error())
+		panic(err.Error())
+	}
+}
+
+func listenNode(conf node, notify chan int) {
+	notify <- 1
 }
 
 func main() {
@@ -40,7 +59,19 @@ func main() {
 	defer seelog.Flush()
 
 	//load all node message
-	loadNodes()
+	nodes := pools{}
+	loadNodes(&nodes)
 
 	//start
+	notify := make(chan int, 1)
+	for i := 0; i < len(nodes.Nodes); i++ {
+		go listenNode(nodes.Nodes[i], notify)
+	}
+
+	for {
+		select {
+		case data := <-notify:
+			fmt.Println("data:", data)
+		}
+	}
 }
